6e69ea6f9f7b2d668810837b4f44cfa399d1a7ac
jjudalori <jjudalori@gmail.com>
2021-12-05 23:32:23 -0500

    Sistema con Laravel

---
+++ resources/views/empleado/index.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+
+@if(Session::has('mensaje'))
+<div class="alert alert-success alert-dismissible" role="alert">
+{{ Session::get('mensaje') }}
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+</button>
+
+</div>
+@endif
+
+
+
+
+<a href="{{ url('empleado/create') }}">Registrar Nuevo Empleado</a>
+
+<table class="table table-light">
+    <thead class="thead-light">
+        <tr>
+            <th>#</th>
+            <th>Foto</th>
+            <th>Nombre</th>
+            <th>Apellido Paterno</th>
+            <th>Apellido Materno</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach( $empleados as $empleado )
+        <tr>
+            <td>{{ $empleado->id }}</td>
+
+            <td>
+                <img src="{{ asset('storage').'/'.$empleado->Foto }}" width="100" alt="">
+                
+            </td>
+
+            <td>{{ $empleado->Nombre }}</td>
+            <td>{{ $empleado->ApellidoPaterno }}</td>
+            <td>{{ $empleado->ApellidoMaterno }}</td>
+            <td>{{ $empleado->Correo }}</td>
+            <td>
+            <a href="{{ url('/empleado/'.$empleado->id.'/edit') }}">
+
+            Editar
+            </a>    
+            
+             | 
+
+            <form action="{{ url('/empleado/'.$empleado->id ) }}" method="post">
+                @csrf
+                {{ method_field('DELETE') }}
+                <input type="submit" onclick="return confirm('¿Quieres Borrar?')" value="Borrar">
+            </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+{!! $empleados->links() !!}
+</div>
+@endsection
